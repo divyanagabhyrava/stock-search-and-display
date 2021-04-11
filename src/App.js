379786c6ff7a1430fc02a4921999b0805ff87f85
divya <divyanagabhyrava2073@gmail.com>
2021-04-11 00:25:12 -0700

    Added axios framework for api calls. Watch list tab will display the list of stocks, Row sub component will display inner details

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { ThemeProvider } from '@material-ui/core/styles';
 import theme from './Components/Theme'
 import {BrowserRouter, Route, Switch} from "react-router-dom"
 
+import WatchList from './Components/WatchList';
+
 function App() {
   return (
     <div className="App">
@@ -11,7 +13,7 @@ function App() {
       <Header/>
       <Switch>
         <Route exact path="/" component={() => <div>Home</div>} />
-        <Route exact path="/watchList" component={() => <div>Watch List</div>} />
+        <Route exact path="/watchList" component={() => <WatchList/>} />
       </Switch>
         </BrowserRouter>
       </ThemeProvider>
